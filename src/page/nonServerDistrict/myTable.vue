346d28ed668bb7704d4789a36b9fbf97802e4937
liqi <liqi@bangbangda.cn>
2017-07-25 11:24:06 +0800

    add table lq 0725

---
+++ src/page/nonServerDistrict/myTable.vue
@@ -0,0 +1,11 @@
+<template type="html">
+   <section class="">
+
+   </section>
+</template>
+<script type="text/javascript">
+    name:'table'
+</script>
+<style lang="scss">
+
+</style>
---
+++ src/page/nonServerDistrict/tableHedader.vue
@@ -0,0 +1,24 @@
+<template type="html">
+     <section class="">
+     </section>
+</template>
+<script type="text/javascript">
+     name:"tableHeader"
+     export default {
+           data(){
+               return {
+
+               }
+           },
+           methods:{
+
+           },
+
+     }
+</script>
+<style lang="scss">
+    .tableHeader {
+         
+
+    }
+</style>
