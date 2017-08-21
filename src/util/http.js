679356da765f42e56528ffe9b643f0c77840462d
liqi <liqi@bangbangda.cn>
2017-08-21 12:08:27 +0800

    lq 0821 7

--- src/util/http.js
+++ src/util/http.js
@@ -17,7 +17,7 @@ let URL = "http://sendexmng-sit.alipay-eco.com"
    // alert(process.env.npm_config_report)
 if(process.env.NODE_ENV === "development"){
     //  开发环境下调用
-    URL = 'http://192.168.12.54:8080'
+    // URL = 'http://192.168.12.54:8080'
     // URL  = "http://sendexmng-sit.alipay-eco.com";
 }else {
     URL = "http://sendexmng-sit.alipay-eco.com"
