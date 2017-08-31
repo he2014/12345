0ceba63a4d2fffad11af5b0f9563556b9d12347d
liqi <liqi@bangbangda.cn>
2017-08-31 14:42:20 +0800

    lq 0831 2

--- src/main.js
+++ src/main.js
@@ -59,14 +59,14 @@ function filterMenu(result) {
               if(pm.CustomerService){
                   filterName = [];
               }else {
-                 filterName = ['订单管理','订单取消频次管理',"黑名单管理","开发者工具"];
+                 filterName = ['订单管理','订单取消频次管理',"黑名单管理","开发者工具","订单批量处理"];
               }
           }else {
             store.dispatch('setAuthority',"审核");
             if(pm.CustomerService){
               filterName = ["开发者工具"];
             }else {
-               filterName = ['订单管理','订单取消频次管理',"黑名单管理","开发者工具"];
+               filterName = ['订单管理','订单取消频次管理',"黑名单管理","开发者工具","订单批量处理"];
             }
           }
    }else {
@@ -75,13 +75,13 @@ function filterMenu(result) {
            if(pm.CustomerService){
              filterName = ["开发者工具"];
            }else {
-              filterName = ['订单管理','订单取消频次管理',"黑名单管理","开发者工具"];
+              filterName = ['订单管理','订单取消频次管理',"黑名单管理","开发者工具","订单批量处理"];
            }
        }else {
          if(pm.CustomerService){
            filterName = ["快递公司管理",'入口管理','运营位管理','公告管理','开发者工具'];
          }else {
-            filterName = ['订单管理','订单取消频次管理',"黑名单管理","快递公司管理",'入口管理','运营位管理','公告管理','开发者工具'];
+            filterName = ['订单管理','订单取消频次管理',"黑名单管理","快递公司管理",'入口管理','运营位管理','公告管理','开发者工具',"订单批量处理"];
          }
        }
    }
